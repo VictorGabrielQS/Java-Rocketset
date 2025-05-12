b715977e4067b03811eb041d48bdeb0b05e37720
Victor2412 <victor.git24@gmail.com>
2025-05-12 07:01:14 -0300

    first commit

---
+++ FUNDAMENTOS_JAVA/PrimeiraClasse.java
@@ -0,0 +1,26 @@
+
+/* 
+ * PrimeiraClasse  = Nome da minha Classe 
+ * public = Tipo de acesso da minha Classe 
+ * class = Tipo da Classe
+ */
+
+public class PrimeiraClasse {
+
+    //Todo conteúdo deverá ser inserido aqui dentro 
+
+    //Método main
+    // public = tipo de acesso do metodo 
+    // static = significa que esse método pertence a essa classe e não uma instancia
+    // void = tipo de retorno do metodo , void significa que não retorna nenhum valor 
+    // main  = precisa ser obirgatorio porque ele que executa o main da classe 
+    // String[] = o tipo que posso receber , aqui esta relatando que posso receber uma lista de Strings  
+    // args = irei receber argumentos 
+    public static void main(String[] args) {
+        
+
+    }
+
+}
+
+//Fora do escpo da Classe
